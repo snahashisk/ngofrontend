de0a9a1f2dcb8abd125101c78da8fdad40a3698c
Snahashis <snahashiskanrar@gmail.com>
2026-06-08 18:11:08 +0530

    feat: implement contact form component and configure axios instance for API communication

--- src/components/shadcn-studio/blocks/register-01/register-form.tsx
+++ src/components/shadcn-studio/blocks/register-01/register-form.tsx
@@ -6,41 +6,91 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import { toast } from "sonner";
+import axiosInstance from "@/lib/axios";
 
 const RegisterForm = () => {
+  const [name, setnNme] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    await axiosInstance
+      .post("/api/v1/contact/createcontactmessage", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      })
+      .then((res) => {
+        toast.success("Message sent successfully");
+        setnNme("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((err) => {
+        toast.error("Something went wrong");
+      });
+  };
+
   return (
-    <form className="space-y-6" onSubmit={(e) => e.preventDefault()}>
-      {/* Username */}
+    <form className="space-y-6" onSubmit={handleSubmit}>
       <div className="space-y-1">
         <Label className="leading-5" htmlFor="username">
           You Name
         </Label>
         <div className="relative">
-          <Input type="text" id="username" placeholder="Enter your name here..." className="h-10 pr-10" />
+          <Input
+            type="text"
+            id="username"
+            placeholder="Enter your name here..."
+            className="h-10 pr-10"
+            value={name}
+            onChange={(e) => setnNme(e.target.value)}
+          />
           <UserIcon className="absolute right-3 top-1/2 -translate-y-1/2 size-4 text-muted-foreground" />
         </div>
       </div>
-
-      {/* Email */}
       <div className="space-y-1">
         <Label className="leading-5" htmlFor="userEmail">
           Your Email Address
         </Label>
         <div className="relative">
-          <Input type="email" id="userEmail" placeholder="Enter your email here..." className="h-10 pr-10" />
+          <Input
+            type="email"
+            id="userEmail"
+            placeholder="Enter your email here..."
+            className="h-10 pr-10"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <MailIcon className="absolute right-3 top-1/2 -translate-y-1/2 size-4 text-muted-foreground" />
         </div>
       </div>
-
-      {/* Message */}
       <div className="space-y-1">
         <Label className="leading-5" htmlFor="message">
           Your Message
         </Label>
-        <Textarea id="message" placeholder="Enter your message here..." className="h-25" />
+        <Textarea
+          id="message"
+          placeholder="Enter your message here..."
+          className="h-25"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
       </div>
-
-      <Button className="w-full h-10 text-base" type="submit" size="icon-lg">
+      <Button
+        className="w-full h-10 text-base cursor-pointer"
+        type="submit"
+        size="icon-lg"
+      >
         Send Message
       </Button>
     </form>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://ngo.kanrarmc.fun",
+  baseURL: "http://localhost:8000",
   withCredentials: true,
 });
 export default axiosInstance;
