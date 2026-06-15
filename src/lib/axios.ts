67feb993f27bf56cce5b037a99ccfcc0ffed1971
Snahashis <snahashiskanrar@gmail.com>
2026-06-15 19:31:09 +0530

    feat: initialize axios instance with base URL and credential support

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
 export default axiosInstance;
